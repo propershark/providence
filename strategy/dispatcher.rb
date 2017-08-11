0c240227db10cd18b65b4171e3f7250575ca7ff4
Elliott Williams <e@elliottwillia.ms>
2017-08-11 15:02:36 -0700

    Add timetable-style invocation logging.

--- strategy/dispatcher.rb
+++ strategy/dispatcher.rb
@@ -24,10 +24,22 @@ module Providence
     def do_visits_between station, start_s, end_s, limit, **_
       start_time = Time.parse(start_s)
       end_time   = Time.parse(end_s)
+      profile_time = Time.now
+      log = proc do |result| 
+        puts <<~EOF
+        Received call to `providence.visits_between`:
+            stop:   #{station}
+            start:  #{start_s}
+            end:    #{end_s}
+            count:  #{limit}
+        Responded in #{(Time.now - profile_time) * 1000}ms with:
+        #{result}
+        EOF
+      end
       EM::DefaultDeferrable.new.tap do |defer|
         @strategies.lazy.map do |strategy| 
           strategy.visits_between(station, start_time, end_time, 
-                                  limit) { |r| defer.succeed r }
+                                  limit) { |r| defer.succeed r; log.call r }
         end.select(&:itself).first
       end
     end
